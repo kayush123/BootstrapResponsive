57965903d0690e24341b1d0c5a5520ca691978f3
AyushIncaendo <ayush@incaendo.com>
2018-02-13 12:07:36 +0530

    A basic layout or scaffholding of bootstrap website

---
+++ venuetravel.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Roux Academy Conference 2013 -- Venue/Travel</title>
+	<meta charset="viewport" content = "width=device-width" initial-scale=1.0>
+	<link href="https://fonts.googleapis.com/css?family=Bree+Serif|Merriweather" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css" media="screen">
+	<link rel="stylesheet" type="text/css" href="_/css/mystyles.css" media="screen">
+</head>
+<body id="venuetravel">
+
+<section class="container">
+	<div class="content row">
+		<section class="main col col-lg-8">
+			<h2>Main Content</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sem nulla pharetra diam sit amet nisl suscipit. </p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sem nulla pharetra diam sit amet nisl suscipit. </p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sem nulla pharetra diam sit amet nisl suscipit. </p>
+		</section>
+		<section class="sidebar col col-lg-4">
+			<h2>Sidebar</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, </p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, </p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, </p>
+		</section>
+	</div>
+</section>
+
+  
+  <script type="text/javascript" src="js/jquery.js"></script>
+  <script type="text/javascript" src="js/bootstrap.js"></script>
+  <script type="text/javascript" src="js/app.js"></script>
+</body>
+</html>
